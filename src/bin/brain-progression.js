5fe7bbbb98ad3faccadb7566e090b9a5bdca73ed
svezr <svezr@yandex.ru>
2020-02-12 22:21:06 +0300

    added game brain-progression
    * subj

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import {
+  createResultObj, generateNumber, Config, startGame,
+} from '../index.js';
+
+const startGameProgression = (numberOfElements = 10) => {
+  const startNumber = generateNumber();
+  const incNumber = generateNumber(2, 9);
+  const indexToHide = generateNumber(3, numberOfElements - 3);
+
+  const resultArr = [startNumber];
+
+  for (let i = 1; i < numberOfElements; i += 1) {
+    resultArr.push(resultArr[i - 1] + incNumber);
+  }
+
+  const correctAnswer = resultArr[indexToHide].toString();
+
+  resultArr[indexToHide] = '..';
+
+  return createResultObj(resultArr.join(' '), correctAnswer);
+};
+
+const gameConfig = {
+  gameFn: startGameProgression,
+  roundCount: 3,
+  gameTerms: 'What number is missing in the progression?',
+  gameQuestion: 'Question: ',
+  gameAnswer: 'Your answer: ',
+  gameLose: '"%userAnswer%" is wrong answer ;(. Correct answer was "%correctAnswer%".\nLet\'s try again, %userName%!',
+  gameRight: 'Correct!',
+  gameWin: 'Congratulations, %userName%!',
+};
+
+startGame(new Config(gameConfig));
