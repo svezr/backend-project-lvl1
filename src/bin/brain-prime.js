738a8ee8b894ffeca8f5ca7b9272b1709e488c15
svezr <svezr@yandex.ru>
2020-02-13 10:30:44 +0300

    Added brain-prime game
    * subj

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import {
+  createResultObj, generateNumber, Config, startGame,
+} from '../index.js';
+
+const isPrime = (numb) => {
+  if (numb % Math.sqrt(numb) === 0) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(numb); i += 1) {
+    if (numb % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const startGamePrime = () => {
+  const numb = generateNumber(2, 99);
+
+  return createResultObj(numb, isPrime(numb) ? 'yes' : 'no');
+};
+
+const gameConfig = {
+  gameFn: startGamePrime,
+  roundCount: 3,
+  gameTerms: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+  gameQuestion: 'Question: ',
+  gameAnswer: 'Your answer: ',
+  gameLose: '"%userAnswer%" is wrong answer ;(. Correct answer was "%correctAnswer%".\nLet\'s try again, %userName%!',
+  gameRight: 'Correct!',
+  gameWin: 'Congratulations, %userName%!',
+};
+
+startGame(new Config(gameConfig));
